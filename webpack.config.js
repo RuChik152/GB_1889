06c5803a7387ab4f784749f84a955afa71748d86
RuChik152 <chernovn152@gmail.com>
2022-04-15 23:18:28 +0300

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path = require( 'path' );
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
-const miniCss = require( "mini-css-extract-plugin" );
+const MiniCssExtractPlugin = require( "mini-css-extract-plugin" );
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const isDev = process.env.NODE_ENV === 'development';
+const withReport = process.env.npm_config_withReport;
 
 module.exports = {
     entry: path.resolve( __dirname, './src/index.jsx' ),
@@ -23,8 +28,37 @@ module.exports = {
                 use: [ 'babel-loader' ],
             },
             {
-                test: /\.scss$/i,
-                use: [ miniCss.loader, "css-loader", "sass-loader" ]
+                test: /\.s?css?$/i,
+                exclude: /\.module\.s?css$/i,
+                use: [
+                    isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                mode: 'icss',
+                                localIdentName: '[name]___[hash:base64:5]',
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.module\.s?css$/,
+                use: [
+                    isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                mode: 'local',
+                                localIdentName: '[name]___[hash:base64:5]',
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -32,14 +66,27 @@ module.exports = {
             }
         ]
     },
-
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()],
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000,
+    },
     plugins: [
         new HtmlWebpackPlugin( {
             template: path.resolve( __dirname, './public/index.html' ),
             filename: 'index.html'
         } ),
-        new miniCss( {
-            filename: 'style.css',
-        } )
+        ...(isDev
+            ? [new MiniCssExtractPlugin()]
+            : [
+                new MiniCssExtractPlugin({
+                    filename: '[name].[contenthash].css',
+                    chunkFilename: '[name].[contenthash].css',
+                }),
+            ]),
+        ...(withReport ? new BundleAnalyzerPlugin() : ''),
     ]
 }
